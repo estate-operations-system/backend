6b942dbad5211ba702a4d737130ae4798671e8aa
PiginIvan <piginivan4@gmail.com>
2026-04-17 20:27:24 +0300

    minor fix

--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -24,7 +24,7 @@ class EmailService {
       },
     };
 
-    this.transporter = nodemailer.createTransporter(config);
+    this.transporter = nodemailer.createTransport(config);
   }
 
   async sendVerificationCode(email: string, code: string, telegramId: string): Promise<void> {
